ec0485f1ed6cef318f15e481d5ce139bc64e5dd0
= <mateusz_pietruczuk07@wp.pl>
2023-09-12 14:25:47 +0200

    Fixed issue with navbar active links.

--- src/js/main.js
+++ src/js/main.js
@@ -22,70 +22,72 @@ const formDiv = document.querySelector(".form-div");
 const callUsDiv = document.querySelector(".call-us-div");
 
 const handleNav = () => {
-  navBtn.classList.toggle("is-active");
-  navMobile.classList.toggle("nav-mobile-active");
-  navMobileBottomLinks.classList.toggle("mobile-bottom-links-active");
+    navBtn.classList.toggle("is-active");
+    navMobile.classList.toggle("nav-mobile-active");
+    navMobileBottomLinks.classList.toggle("mobile-bottom-links-active");
 };
 const handleMobileNavItems = () => {
-  navBtn.classList.remove("is-active");
-  navMobile.classList.remove("nav-mobile-active");
+    navBtn.classList.remove("is-active");
+    navMobile.classList.remove("nav-mobile-active");
 };
 const handleScroll = () => {
-  allSections.forEach((section) => {
-    let top = window.scrollY;
-    let offset = section.offsetTop;
-    let height = section.offsetHeight;
-    let sectionId = section.getAttribute("id");
+    allSections.forEach((section) => {
+        let top = window.scrollY;
+        let offset = section.offsetTop;
+        let height = section.offsetHeight;
+        let sectionId = section.getAttribute("id");
 
-    if (top >= offset && top < offset + height) {
-      allNavLinks.forEach((link) => {
-        link.classList.remove("bottom-nav-link-active");
-        document
-          .querySelector("header nav a[href*=" + sectionId + "]")
-          .parentElement.classList.add("bottom-nav-link-active");
-      });
-    }
-  });
+        if (top >= offset && top < offset + height) {
+            allNavLinks.forEach((link) => {
+                link.classList.remove("bottom-nav-link-active");
+                document
+                    .querySelector("header nav a[href*=" + sectionId + "]")
+                    .parentElement.classList.add("bottom-nav-link-active");
+            });
+        }
+    });
 };
+
 function openAccordionItems() {
-  if (this.nextElementSibling.classList.contains("active")) {
-    this.nextElementSibling.classList.remove("active");
-  } else {
-    closeAccordionItem();
-    this.nextElementSibling.classList.toggle("active");
-  }
+    if (this.nextElementSibling.classList.contains("active")) {
+        this.nextElementSibling.classList.remove("active");
+    } else {
+        closeAccordionItem();
+        this.nextElementSibling.classList.toggle("active");
+    }
 }
+
 const closeAccordionItem = () => {
-  const allActiveItems = document.querySelectorAll(".accordion-info");
-  allActiveItems.forEach((item) => item.classList.remove("active"));
+    const allActiveItems = document.querySelectorAll(".accordion-info");
+    allActiveItems.forEach((item) => item.classList.remove("active"));
 };
 const clickOutsideAccordion = (e) => {
-  if (
-    e.target.classList.contains("accordion-btn") ||
-    e.target.classList.contains("accordion-info") ||
-    e.target.classList.contains("accordion-info-text")
-  )
-    return;
+    if (
+        e.target.classList.contains("accordion-btn") ||
+        e.target.classList.contains("accordion-info") ||
+        e.target.classList.contains("accordion-info-text")
+    )
+        return;
 };
 const handleContactDiv = () => {
-  contactFormBtn.classList.toggle("btn-active");
-  formDiv.classList.toggle("form-active");
-  callUsBtn.classList.toggle("btn-active");
-  callUsDiv.classList.toggle("call-us-active");
+    contactFormBtn.classList.toggle("btn-active");
+    formDiv.classList.toggle("form-active");
+    callUsBtn.classList.toggle("btn-active");
+    callUsDiv.classList.toggle("call-us-active");
 };
 
 navBtn.addEventListener("click", handleNav);
 
 allNavItems.forEach((item) => {
-  item.addEventListener("click", handleMobileNavItems);
+    item.addEventListener("click", handleMobileNavItems);
 });
 
 dropdownBtn.addEventListener("click", () => {
-  dropdownList.classList.toggle("active");
+    dropdownList.classList.toggle("active");
 });
 
 accordionBtns.forEach((btn) =>
-  btn.addEventListener("click", openAccordionItems),
+    btn.addEventListener("click", openAccordionItems),
 );
 
 contactFormBtn.addEventListener("click", handleContactDiv);
