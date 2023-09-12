4dd2435f8fad5ff22a7ebf5889a8caa120c80239
= <mateusz_pietruczuk07@wp.pl>
2023-09-12 12:03:34 +0200

    Code cleaning, small fixes and improvements.

--- src/js/slick.js
+++ src/js/slick.js
@@ -64,6 +64,13 @@ $(".product-family-card-wrapper").slick({
     {
       breakpoint: 576,
       settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 992,
+      settings: {
         slidesToShow: 2,
         slidesToScroll: 1,
       },
